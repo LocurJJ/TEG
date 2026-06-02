ba0fd1984695d891c52fb710ebd62a9d7fbc789d
LocurJJ <127799708+LocurJJ@users.noreply.github.com>
2026-06-02 08:56:06 -0300

    Agregar configuracion de Firebase

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+window.TEG_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC6TNgPOI2Hv3qGKMpwj8UdP-YB23ZHmHg",
+  authDomain: "teg2-cad5b.firebaseapp.com",
+  databaseURL: "https://teg2-cad5b-default-rtdb.firebaseio.com",
+  projectId: "teg2-cad5b",
+  storageBucket: "teg2-cad5b.firebasestorage.app",
+  messagingSenderId: "547485089345",
+  appId: "1:547485089345:web:06ac44497f027c6d8610c0",
+  measurementId: "G-B2W57CZEBJ"
+};
+
+// Realtime Database configurada para jugar online.
